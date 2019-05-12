bed34f35ad44fd7462853a7f2a599fc9ecda72ee
margaretmwaura <mwauramargaret1@gmail.com>
2019-05-12 08:58:16 +0300

    Properly exiting the application

--- app/src/admin/java/com/example/admin/chamaapp/Sign.java
+++ app/src/admin/java/com/example/admin/chamaapp/Sign.java
@@ -95,7 +95,11 @@ public class Sign extends AppCompatActivity
 
     }
 
-
-
-
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
 }
